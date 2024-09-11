fddec6cabf240b2442d9887977db987369d75b5c
R-y-a-n8 <ryan.kinuthia@student.moringaschool.com>
2024-09-11 22:51:17 +0300

    aggregate

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}  
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -15,9 +17,21 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        """Counts the number of courses a student is enrolled in."""
+        return len(self._enrollments)
+
+    def aggregate_average_grade(self):
+        """Calculates the average grade across all enrollments."""
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        if num_courses == 0:
+            return 0  
+        return total_grades / num_courses
+
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -45,3 +59,12 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        """Counts enrollments per day."""
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
